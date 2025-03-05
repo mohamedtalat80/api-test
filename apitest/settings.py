9b6ce2ead471d2941c9d540f567fa001f06eef8e
mohamed <abnmoh525@gmail.com>
2025-03-05 22:14:58 +0200

    adding the htmls files

--- apitest/settings.py
+++ apitest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!#qz5a%90!j-si1qq5qww0t@w3pb$59=1pqutcq=+x#u03o#cf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
